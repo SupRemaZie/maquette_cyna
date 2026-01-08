024826bde979c4db26d90556df4a2e7e7f93cdc3
SupRemaZie <Damien.Porchet.Etu@univ-lemans.fr>
2026-01-08 18:03:18 +0100

    fix calendar

--- src/components/ui/calendar.jsx
+++ src/components/ui/calendar.jsx
@@ -3,14 +3,12 @@ import { ChevronLeft, ChevronRight } from "lucide-react"
 import { DayPicker } from "react-day-picker"
 import { cn } from "../../lib/utils"
 
-export type CalendarProps = React.ComponentProps<typeof DayPicker>
-
 function Calendar({
   className,
   classNames,
   showOutsideDays = true,
   ...props
-}: CalendarProps) {
+}) {
   return (
     <DayPicker
       showOutsideDays={showOutsideDays}
